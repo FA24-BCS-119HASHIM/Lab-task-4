02280bc7b098ebbbc0dba4636cf218eb167c7854
FA24-BCS-119HASHIM <hashimx441@gmail.com>
2025-03-16 16:37:31 +0500

    Update Lab.java

--- Campus management system/Lab.java
+++ Campus management system/Lab.java
@@ -1,28 +1,3 @@
-/*public class lab {
-    String name;
-    Person Incharge;
-    int[] num;
-lab() {
-    this.name = "Computer Lab"; 
-    Incharge = new Person("Hash", 1234); 
-    num = new int[50]; 
-
-    for (int i = 0; i < num.length; i++) {
-        num[i] = i + 1; 
-    }
-}
-
-// Display function to print the details
-void display() {
-    System.out.println("Lab Name: " + name);
-    System.out.println("Incharge Name: " + Incharge.name);
-    System.out.println("Incharge ID: " + Incharge.id);
-    System.out.println("PC number:");
-    for (int i = 0; i < 50; i++) {
-        System.out.println("Number at index " + i + ": " + num[i]);
-    }
-}
-}*/
 class Lab {
     String name;
     Person incharge;
