192531db8d382c2b111f657d2403ee44795c86d1
FA24-BCS-119HASHIM <hashimx441@gmail.com>
2025-03-16 16:32:07 +0500

    Update Computer.java

--- Campus management system/Computer.java
+++ Campus management system/Computer.java
@@ -1,27 +1,3 @@
-/*public class Computer {
-    int id;
-    String version;
-    String Ram;
-    String Disksize;
-    lab Lab=new lab();
-
-    Computer(int id,String version,String Ram,String Disksize){
-        this.id=id;
-        this.version=version;
-        this.Ram=Ram;
-        this.Disksize=Disksize;
-}
-    void display(){
-        Lab.display();
-
-
-        System.out.println("Id :"+id);
-        System.out.println("Version :"+version);
-        System.out.println("Ram"+Ram);
-        System.out.println("Disk size :"+Disksize);
-    }
-
-}*/
 public class Computer {
     int id;
     String version;
